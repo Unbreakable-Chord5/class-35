0836f4eecad3cec04475e72604e441622148805a
Unbreakable-Chord5 <82305161+Unbreakable-Chord5@users.noreply.github.com>
2021-08-29 12:06:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var hypnopicBall,database,position;
+
+function setup(){
+    database=firebase.database();
+    console.log(database);
+    createCanvas(500,500);
+    hypnopicBall = createSprite(250,250,10,10);
+
+    var hypnopicBallPositon = database.ref('ball/position');
+    hypnopicBallPositon.on("value",readPosition,showError)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
+function writePosition(x,y){
+    database.ref('ball/position').setup({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+}
